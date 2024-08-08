14ec4feab86f240711600f972de389e2f07a662a
glrodriguess <guilima456huebr@gmail.com>
2024-08-07 21:54:08 -0300

    Add files via upload

---
+++ primeiro.py
@@ -0,0 +1,10 @@
+# pessoa = input("Digite o nome da pessoa: ")
+# print("Boa noite", pessoa)
+# idade = input ("Qual a sua idade? ")
+# print("Nossa! voce já tem", idade, "anos")
+# email = input ("Qual o seu email? ")
+# print("Seu email é",email)
+
+numero = input("Digite um número: ")
+outroNumero = input("Digite outro número: ")
+print("A soma é", float(numero) + float(outroNumero))
---
+++ tabuada.py
@@ -0,0 +1,3 @@
+tabuada = int(input("Qual a tabuada: "))
+for contador in range(11):
+    print(" A multiplicacao de",contador,"*",tabuada,"=",contador*tabuada)
